9282315cadbf03336398e7603d559d97b5037452
bhitt <bhitt@student.rcc.edu>
2016-09-06 23:24:48 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

---
+++ Review/build/Debug/Cygwin_4.x-Windows/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin_4.x-Windows/main.o: main.cpp Prob3Table.h
+
+Prob3Table.h:
---
+++ Review1/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * File:   main.cpp
+ * Author: Branden Hitt
+ * Created on September 6th, 2016, 8:40 PM
+ *      Purpose: Review
+ */
+#include <QCoreApplication>
+//System Libraries
+#include <iostream> //I/O standard
+#include <fstream> //file I/O
+using namespace std;
+
+//User Libraries
+#include "Prob3Table.h"
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    cout<<"Entering problem number 3"<<endl;
+        int rows=5;
+        int cols=6;
+        Prob3TableInherited<int> tab("Problem3.txt",rows,cols);
+        const int *naugT=tab.getTable();
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                cout<<naugT[i*cols+j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+        const int *augT=tab.getAugTable();
+        for(int i=0;i<=rows;i++)
+        {
+            for(int j=0;j<=cols;j++)
+            {
+                cout<<augT[i*(cols+1)+j]<<" ";
+            }
+            cout<<endl;
+        }
+        return 0;
+
+    return a.exec();
+}

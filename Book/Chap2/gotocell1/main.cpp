7b9a2d089a27c9b341f2ef0f47a5506f70321ab7
bhitt <bhitt@student.rcc.edu>
2016-09-25 22:27:14 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Book/Chap2/gotocell1/main.cpp
+++ Book/Chap2/gotocell1/main.cpp
@@ -1,16 +1,16 @@
 #include <QApplication>
-#include <QGui>
 #include <QDialog>
 
 #include "ui_gotocelldialog.h"
 
-int main(int argc, char *arg[]){
-    QApplication app(argc,argv);
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
 
     Ui::GoToCellDialog ui;
     QDialog *dialog = new QDialog;
     ui.setupUi(dialog);
     dialog->show();
 
-    return app.exec();
+    return a.exec();
 }
---
+++ Book/Chap2/gotocell3/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+
+#include "gotocelldialog.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    GoToCellDialog *dialog = new GoToCellDialog;
+    dialog->show();
+
+    return a.exec();
+}
